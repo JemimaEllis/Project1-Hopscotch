16557b62f12ccfb60f952f2962429c057a29303d
JemimaEllis <jemimaeellis@gmail.com>
2017-09-18 09:57:17 +0100

    weekend changes

--- main.js
+++ main.js
@@ -1,2 +1,54 @@
-var numbers = ["5", "2", "8", "1", "4", "8", "1", "4", "7", "1", "9", "3", "6"];
-console.log(numbers[1]);
+// var numbers = ["math.randomize"];
+// console.log(numbers[0]);
+
+// alert ("HOPSCOTCH \n \n \nINSTRUCTIONS:\n \nAIM OF GAME: Get to the finish line as fast as possible. Type in the number on the square your character is currently on to proceed. Finish in the fastes time possible.");
+
+
+$(function (){
+
+
+
+var $boxes = $(".box");
+
+// for(var i = 0; i < $boxes.length; i++) {
+
+// // console.log(i);
+// 	// $("#box" + i).html(Math.random);
+// 	$boxes:eq(i).html(getRandomNumber);
+
+
+
+// // }
+
+
+
+$boxes.each(function(event) {
+
+	$(this).html(getRandomNumber ());
+
+});
+
+});
+
+
+
+function getRandomNumber () {
+	return Math.floor(Math.random() * 9) + 1;
+
+}
+
+
+// $("#regTitle").html("Hello World");
+
+
+// for (i = 0; i < cars.length; i++) { 
+//     text += cars[i] + "<br>";
+// }
+
+
+
+
+// grab all of the boxes and store them in a variable. 
+
+// loop through each box, change the innerHTML to matching index in the numbers array.
+
